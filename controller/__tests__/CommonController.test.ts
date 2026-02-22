966b40641b56d95260bcca2db8afc023310c688c
PuffRice <drytwerz69@gmail.com>
2026-02-22 20:59:09 +0600

    Deleted unnecessary files, added .gitignore

--- controller/__tests__/CommonController.test.ts
+++
@@ -1,7 +0,0 @@
-import * as common from '../CommonController';
-
-describe('CommonController (types-only)', () => {
-  it('has no runtime exports', () => {
-    expect(Object.keys(common).length).toBe(0);
-  });
-});
