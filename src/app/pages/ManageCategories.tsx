783c2e64f61cad6bd7ef40ab08487bb2ee3c744a
PuffRice <drytwerz69@gmail.com>
2026-02-26 22:14:34 +0600

    Minor Fixes

--- src/app/pages/ManageCategories.tsx
+++ src/app/pages/ManageCategories.tsx
@@ -371,7 +371,7 @@ export function ManageCategories() {
         </div>
       )}
 
-      {/* Add Modal */}
+      {/* g Modal */}
       {addingCategory && (
         <div className="fixed inset-0 bg-black/50 flex items-center justify-center z-50">
             <div className="bg-card rounded-lg shadow-xl w-full max-w-md p-6">
